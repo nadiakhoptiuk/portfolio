2601d379c996f7e557cb162dcb2a16a515a50d0c
Nadiia Khoptiuk <holineinadia@gmail.com>
2024-05-06 22:49:33 +0300

    add device sizes

--- next.config.js
+++ next.config.js
@@ -10,6 +10,7 @@ const nextConfig = {
   },
   images: {
     formats: ['image/avif', 'image/webp'],
+    deviceSizes: [480, 640, 750, 828, 1080, 1200, 1440, 1920, 2048, 3840],
     remotePatterns: [{ protocol: 'https', hostname: 'res.cloudinary.com' }],
   },
 };
